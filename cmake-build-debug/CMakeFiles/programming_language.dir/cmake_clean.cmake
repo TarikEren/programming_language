ceffa47653ad8521ed9b1643792b53a9531c4f84
TarikEren <1210606015@nku.edu.tr>
2025-08-04 19:05:58 +0300

    Added build

---
+++ cmake-build-debug/CMakeFiles/programming_language.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/programming_language.dir/Source/Lexer.cpp.o"
+  "CMakeFiles/programming_language.dir/Source/Lexer.cpp.o.d"
+  "CMakeFiles/programming_language.dir/main.cpp.o"
+  "CMakeFiles/programming_language.dir/main.cpp.o.d"
+  "programming_language"
+  "programming_language.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/programming_language.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
